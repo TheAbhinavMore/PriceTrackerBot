5a31edc028986f190ae310d2f809e5306b23df3a
TheAbhinavMore <132703471+TheAbhinavMore@users.noreply.github.com>
2023-09-11 01:34:53 +0530

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -11,75 +11,85 @@ tele_token = os.environ['telegram_token']
 
 def get_random_headers(site=None):
   user_agent_list = [
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'
   ]
 
   headers = {
-      'User-Agent': random.choice(user_agent_list),
-      'Accept-Language': 'en-US',
-      'pin': '400020',
-      'X-Requested-With': 'XMLHttpRequest',
-      'X-KL-kfa-Ajax-Request': 'Ajax_Request',
-      'Connection': 'keep-alive',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      'TE': 'trailers'
+    'User-Agent': random.choice(user_agent_list),
+    'Accept-Language': 'en-US',
+    'pin': '400020',
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-KL-kfa-Ajax-Request': 'Ajax_Request',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'TE': 'trailers'
   }
 
+  # ua = UserAgent()
+  # headers = {'User-Agent': ua.random}
   return headers
 
-
 def cleanLink(text):
-  # Find the first valid URL pattern in the text
-  match = re.search(r'(https?://[^\s]+|www\.[^\s]+|[^\s]+\.[^\s]+)', text)
-  if match:
-    url = match.group(0)
-    if not url.startswith(('http', 'www')):
-      url = f'https://{url}'
-
-    try:
-      response = requests.get(url,
-                              headers=get_random_headers(),
-                              allow_redirects=True)
-
-      if response.history:
-        # Use the final URL after following redirects
-        final_url = response.url
+  # Check for https:// link
+  https_match = re.search(r'https?://[^\s]+', text)
+  if https_match:
+    url = https_match.group(0)
+  else:
+    # Check for http:// link
+    http_match = re.search(r'http://[^\s]+', text)
+    if http_match:
+      url = http_match.group(0)
+    else:
+      # Check for www link
+      www_match = re.search(r'www\.[^\s]+', text)
+      if www_match:
+        url = www_match.group(0)
+        url = f'https://{url}'  # Add 'https://' if it starts with 'www'
       else:
-        final_url = url
+        return None
 
-      parsed_url = urlparse(final_url)
-      cleaned_url = urlunparse(
-          (parsed_url.scheme, parsed_url.netloc, parsed_url.path, '', '', ''))
+  try:
+    response = requests.get(url,
+                            headers=get_random_headers(),
+                            allow_redirects=True)
 
-      if 'amazon' in cleaned_url:
-        cleaned_url = re.sub(r'/ref=.*', '', cleaned_url)
-      elif 'flipkart' in cleaned_url:
-        query_params = parse_qs(parsed_url.query)
-        pid_param = query_params.get('pid')
-        if pid_param:
-          pid_string = f'?pid={pid_param[0]}'
-          cleaned_url += pid_string
+    if response.history:
+      # Use the final URL after following redirects
+      final_url = response.url
+    else:
+      final_url = url
 
-      return cleaned_url
+    parsed_url = urlparse(final_url)
+    cleaned_url = urlunparse(
+      (parsed_url.scheme, parsed_url.netloc, parsed_url.path, '', '', ''))
 
-    except requests.RequestException:
-      return None
+    if 'amazon' in cleaned_url:
+      cleaned_url = re.sub(r'/ref=.*', '', cleaned_url)
+    elif 'flipkart' in cleaned_url:
+      query_params = parse_qs(parsed_url.query)
+      pid_param = query_params.get('pid')
+      if pid_param:
+        pid_string = f'?pid={pid_param[0]}'
+        cleaned_url += pid_string
 
-  return None
+    return cleaned_url
+
+  except requests.RequestException:
+    return None
 
 
 # import this list whenever neccessary for convieniect ex.telegram.py> 455
 supported_sites = [
-    'Amazon', 'Flipkart', 'Snapdeal', 'Ajio', 'Nykaa', '1mg', 'Bewakoof',
-    'Netmeds', 'MD-Computers', 'EZPZSolutions', 'TPStech', 'PC-Studio',
-    'Primeabgb', 'Vedant-computers'
+  'Amazon', 'Flipkart', 'Snapdeal', 'Ajio', 'Nykaa', '1mg', 'Bewakoof',
+  'Netmeds', 'MD-Computers', 'EZPZSolutions', 'TPStech', 'PC-Studio',
+  'Primeabgb', 'Vedant-computers'
 ]
 
 
